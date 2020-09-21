c1ffb0b091e15a8c5833cb601b9f212fe75aaee1
Andy Jones <aejones89@gmail.com>
2020-09-21 18:27:05 +0200

    Add layout for work pages

---
+++ layouts/work-page.js
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+
+const WorkPage = ({ children, frontMatter }) => {
+  return (
+    <Layout showHeader={true} theme="white">
+      <div className="my-16">
+        <h1 className="mb-8 font-mono font-extrabold text-lg leading-9 tracking-tight">
+          {frontMatter.title}
+        </h1>
+        {children}
+      </div>
+    </Layout>
+  )
+}
+
+export default WorkPage
