1f925d3956039d6fab95f6dbd1d6fae4a30aabc5
Andy Jones <aejones89@gmail.com>
2021-03-05 12:06:41 +0000

    React Aws Terraform Github Actions Article (#3)
    * Start writing article
    
    * Add some notes on the deployment pipeline
    
    * Write the bulk of the article
    
    * Add Plausible Analytics

--- components/SiteHead.js
+++ components/SiteHead.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 
-const SiteHead = () => (
+const SiteHead = ({ title  }) => (
   <Head>
-    <title>Andy Jones</title>
+    <title>Andy Jones {title && ` - ${title}`}</title>
     <link rel="icon" href="/favicon.ico" />
+    <script async defer data-domain="andyjones.co" src="https://plausible.io/js/plausible.js"></script>
   </Head>
 )
 
